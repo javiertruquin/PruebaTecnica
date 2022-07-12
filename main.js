cc91cc1c4f3969359d424a3f581da3c1780eea8a
javier Truquin <javiertruquin@gmail.com>
2022-07-11 23:14:21 -0300

    reduccion de consultas a la API al minimo posible

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const tituloH1 = document.getElementById("titulo");
 const formularioForm = document.getElementById("formulario");
 const textoInput = document.getElementById("inputTexto");
 const listaProductos = document.getElementById("lista");
 const categoriaSelect = document.getElementById("selectCat");
 const filtroObj = document.getElementById("filtro");
-const filtroSelect = document.getElementById("selectFiltro");
+const detalleDiv = document.getElementById("detalleProducto");
+
+var productosBuscados = [];
 
 function categoriasProductosTienda() {
     $.ajax({
@@ -51,18 +52,19 @@ formularioForm.onsubmit = function buscarProductosTienda(e) {
             ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
         },
         success: function (resultados) {
-        console.log("buscarProductosTienda ~ resultados", resultados.resultado.Table)
+            productosBuscados = resultados.resultado.Table;
             let filtro = [];
             let filtroBoton = `
-                        <h4 class="my-auto mx-2">Ordenar por:</h4>
-            <select id="selectFiltro" class="select-texto">
-            <option selected>Por nombre</option>
-            <option >Por menor precio</option>
-            </select>
-            <button class="mx-2 btn btn-primary my-3 my-sm-0" onclick="ordenarProductos(selectFiltro.value)"><h4 class="my-auto">Ordenar</h4></button>`;
+                <h4 class="my-auto mx-2">Ordenar por:</h4>
+                <select id="selectFiltro" class="texto-med">
+                    <option selected>Por nombre</option>
+                    <option >Por menor precio</option>
+                </select>
+                <button class="mx-2 btn btn-primary my-3 my-sm-0" onclick="ordenarProductos(selectFiltro.value)"><h4 class="my-auto">Ordenar</h4></button>
+                `;
             filtro.push(filtroBoton);
             filtroObj.innerHTML = filtro.join("");
-            resultados.resultado.Table.sort((a, b) => {
+            productosBuscados.sort((a, b) => {
                 if (a.NOMBRE == b.NOMBRE) {
                     return 0;
                 }
@@ -72,17 +74,17 @@ formularioForm.onsubmit = function buscarProductosTienda(e) {
                 return 1;
             });
             let cards = [];
-            for (let i = 0; i < resultados.resultado.Table.length; i++) {
-                var producto = resultados.resultado.Table[i];
+            for (let i = 0; i < productosBuscados.length; i++) {
+                let producto = productosBuscados[i];
                 let cardProducto = `
                 <div class="card m-3">
                     <div class="d-flex">
-                        <img onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                            class="p-4 " src="
+                        <img onclick="mostrarDetalle('${producto.COD_PRODUCTO_TIENDA}')" data-toggle="modal" data-target="#modalDetalle"
+                            class="p-4 puntero-dedo" src="
                             https://telemedicina.jakemate.net:7141/api${producto.IMAGEN}" alt="">
                         <div class="card-body my-auto">
-                            <p onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                                class="card-text">${producto.NOMBRE}</p>
+                            <p onclick="mostrarDetalle('${producto.COD_PRODUCTO_TIENDA}')" data-toggle="modal" data-target="#modalDetalle"
+                                class="card-text puntero-dedo">${producto.NOMBRE}</p>
                             <h5 class="card-title"><b>${producto.PRECIO},00 pesos</b></h5>
                             <p class="card-text">${producto.CATEGORIA_PROD_TIENDA}</p>
                         </div>
@@ -98,145 +100,55 @@ formularioForm.onsubmit = function buscarProductosTienda(e) {
 
 function ordenarProductos(filtro) {
     if (filtro == "Por nombre") {
-        let tnombre = textoInput.value;
-        let tcat = categoriaSelect.value;
-        $.ajax({
-            url: "https://telemedicina.jakemate.net:7141/api/webservice/metodo",
-            data: {
-                _nombreMetodo_: "buscarProductosTienda",
-                NOMBRE: `${tnombre}`,
-                CATEGORIA: `${tcat}`,
-            },
-            method: "POST",
-            headers: {
-                Token: "NJKJNTL8SNKH5JJRTS32ZGSIIDPGHLU6KRXLQMLMJJU8MD7EY5TSWMGD2D6Z",
-                ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
-            },
-            success: function (resultados) {
-                let filtro = [];
-                let filtroBoton = `
-                        <h4 class="my-auto mx-2">Ordenar por:</h4>
-            <select id="selectFiltro" class="select-texto">
-            <option selected>Por nombre</option>
-            <option >Por menor precio</option>
-            </select>
-            <button class="mx-2 btn btn-primary my-3 my-sm-0" onclick="ordenarProductos(selectFiltro.value)"><h4 class="my-auto">Ordenar</h4></button>`;
-                filtro.push(filtroBoton);
-                filtroObj.innerHTML = filtro.join("");
-                resultados.resultado.Table.sort((a, b) => {
-                    if (a.NOMBRE == b.NOMBRE) {
-                        return 0;
-                    }
-                    if (a.NOMBRE < b.NOMBRE) {
-                        return -1;
-                    }
-                    return 1;
-                });
-                let cards = [];
-                for (let i = 0; i < resultados.resultado.Table.length; i++) {
-                    var producto = resultados.resultado.Table[i];
-                    let cardProducto = `
-                <div class="card m-3">
-                    <div class="d-flex">
-                        <img onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                            class="p-4 " src="
-                            https://telemedicina.jakemate.net:7141/api${producto.IMAGEN}" alt="">
-                        <div class="card-body my-auto">
-                            <p onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                                class="card-text">${producto.NOMBRE}</p>
-                            <h5 class="card-title"><b>${producto.PRECIO},00 pesos</b></h5>
-                            <p class="card-text">${producto.CATEGORIA_PROD_TIENDA}</p>
-                        </div>
-                    </div>
-                </div>
-            `;
-                    cards.push(cardProducto);
-                }
-                listaProductos.innerHTML = cards.join("");
-            },
+        productosBuscados.sort((a, b) => {
+            if (a.NOMBRE == b.NOMBRE) {
+                return 0;
+            }
+            if (a.NOMBRE < b.NOMBRE) {
+                return -1;
+            }
+            return 1;
         });
     } else {
-        let tnombre = textoInput.value;
-        let tcat = categoriaSelect.value;
-        $.ajax({
-            url: "https://telemedicina.jakemate.net:7141/api/webservice/metodo",
-            data: {
-                _nombreMetodo_: "buscarProductosTienda",
-                NOMBRE: `${tnombre}`,
-                CATEGORIA: `${tcat}`,
-            },
-            method: "POST",
-            headers: {
-                Token: "NJKJNTL8SNKH5JJRTS32ZGSIIDPGHLU6KRXLQMLMJJU8MD7EY5TSWMGD2D6Z",
-                ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
-            },
-            success: function (resultados) {
-                let filtro = [];
-                let filtroBoton = `
-                        <h4 class="my-auto mx-2">Ordenar por:</h4>
-            <select id="selectFiltro" class="select-texto">
-            <option >Por nombre</option>
-            <option selected>Por menor precio</option>
-            </select>
-            <button class="mx-2 btn btn-primary my-3 my-sm-0" onclick="ordenarProductos(selectFiltro.value)"><h4 class="my-auto">Ordenar</h4></button>`;
-                filtro.push(filtroBoton);
-
-                filtroObj.innerHTML = filtro.join("");
-
-                resultados.resultado.Table.sort((a, b) => {
-                    if (a.PRECIO == b.PRECIO) {
-                        return 0;
-                    }
-                    if (a.PRECIO < b.PRECIO) {
-                        return -1;
-                    }
-                    return 1;
-                });
-                let cards = [];
-                for (let i = 0; i < resultados.resultado.Table.length; i++) {
-                    var producto = resultados.resultado.Table[i];
-                    let cardProducto = `
-                <div class="card m-3">
-                    <div class="d-flex">
-                        <img onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                            class="p-4" src="
-                            https://telemedicina.jakemate.net:7141/api${producto.IMAGEN}" alt="">
-                        <div class="card-body my-auto">
-                            <p onclick="mostrarDetalle('${producto.NOMBRE}')" data-toggle="modal" data-target="#modalDetalle"
-                                class="card-text">${producto.NOMBRE}</p>
-                            <h5 class="card-title"><b>${producto.PRECIO},00 pesos</b></h5>
-                            <p class="card-text">${producto.CATEGORIA_PROD_TIENDA}</p>
-                        </div>
-                    </div>
-                </div>
-            `;
-                    cards.push(cardProducto);
-                }
-                listaProductos.innerHTML = cards.join("");
-            },
+        productosBuscados.sort((a, b) => {
+            if (a.PRECIO == b.PRECIO) {
+                return 0;
+            }
+            if (a.PRECIO < b.PRECIO) {
+                return -1;
+            }
+            return 1;
         });
     }
+    let cards = [];
+    for (let i = 0; i < productosBuscados.length; i++) {
+        let producto = productosBuscados[i];
+        let cardProducto = `
+            <div class="card m-3">
+                <div class="d-flex">
+                    <img onclick="mostrarDetalle('${producto.COD_PRODUCTO_TIENDA}')" data-toggle="modal" data-target="#modalDetalle"
+                        class="p-4 puntero-dedo" src="
+                        https://telemedicina.jakemate.net:7141/api${producto.IMAGEN}" alt="">
+                    <div class="card-body my-auto">
+                        <p onclick="mostrarDetalle('${producto.COD_PRODUCTO_TIENDA}')" data-toggle="modal" data-target="#modalDetalle"
+                            class="card-text puntero-dedo">${producto.NOMBRE}</p>
+                        <h5 class="card-title"><b>${producto.PRECIO},00 pesos</b></h5>
+                        <p class="card-text">${producto.CATEGORIA_PROD_TIENDA}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        cards.push(cardProducto);
+    }
+    listaProductos.innerHTML = cards.join("");
 }
 
-function mostrarDetalle(nombre) {
-    let tnombre = nombre;
-    $.ajax({
-        url: "https://telemedicina.jakemate.net:7141/api/webservice/metodo",
-        data: {
-            _nombreMetodo_: "buscarProductosTienda",
-            NOMBRE: `${tnombre}`,
-            CATEGORIA: "",
-        },
-        method: "POST",
-        headers: {
-            Token: "NJKJNTL8SNKH5JJRTS32ZGSIIDPGHLU6KRXLQMLMJJU8MD7EY5TSWMGD2D6Z",
-            ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
-        },
-        success: function (resultados) {
-            let producto = resultados.resultado.Table[0];
-            console.log("mostrarDetalle ~ producto", producto.IMAGEN);
-            const detalleDiv = document.getElementById("detalleProducto");
-            const detalleProducto = `
+function mostrarDetalle(codigo) {
+    let productoSeleccionado = productosBuscados.filter(
+        (n) => n.COD_PRODUCTO_TIENDA == codigo
+    );
+    let producto = productoSeleccionado[0];
+    const detalleProducto = `
                         <div class="d-flex">
                         <img class="w-50"
                             src="
@@ -258,7 +170,5 @@ function mostrarDetalle(nombre) {
                     </div>
                     <p class="ml-3 mb-0">${producto.DESCRIPCION}</p>
                         `;
-            detalleDiv.innerHTML = detalleProducto;
-        },
-    });
+    detalleDiv.innerHTML = detalleProducto;
 }
